b37ff1f27bac3c20081f090c7bd0e1308d9503e2
bharaninrb <bharaninrb2002@gmail.com>
2024-06-23 14:22:39 +0530

    init commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/**
+ * Initiate portfolio lightbox
+ */
+const portfolioLightbox = GLightbox({
+  selector: ".portfolio-lightbox",
+});
+
+/**
+ * Initiate portfolio details lightbox
+ */
+const portfolioDetailsLightbox = GLightbox({
+  selector: ".portfolio-details-lightbox",
+  width: "90%",
+  height: "90vh",
+});
